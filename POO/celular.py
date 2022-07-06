bef6e8d222fbfea45dc249895c5ca8f876a8214e
maurovmorais <maurovmorais@gmail.com>
2022-07-05 21:41:01 -0300

    Primeiro commit

---
+++ POO/celular.py
@@ -0,0 +1,16 @@
+class Celular:
+    marca = 'Nokia'
+    modelo = 'Tijoão'
+    cor = 'Azul'
+    tem_camera = 'Não'
+    bateria = 'Infinita'
+
+    def fazer_ligacao(self):
+        print('Fazendo ligação...')
+
+    def jogar_cobrinha(self):
+        print('Jogando cobrinha...')
+
+    def despertador(self):
+        print('Despertando...')
+
---
+++ POO/main.py
@@ -0,0 +1,13 @@
+from operator import imod
+from celular import *
+
+Celular = Celular()
+print(Celular.marca)
+print(Celular.modelo)
+print(Celular.cor)  
+print(Celular.tem_camera)
+print(Celular.bateria)
+
+Celular.fazer_ligacao()
+Celular.despertador()
+Celular.jogar_cobrinha()
